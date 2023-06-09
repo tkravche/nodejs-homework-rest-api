ace227d9ac9dff3a5de11b6f0cc2f7659bf7ee27
Tanya <tkravche@yahoo.com>
2023-06-09 16:45:33 +0200

    hw3  steps 1-3

--- routes/api/contacts-router.js
+++ routes/api/contacts-router.js
@@ -3,19 +3,19 @@ const express = require("express");
 const contactsController = require("../../controllers/contacts-controller");
 
 const {validateBody} = require("../../decorators");
-
+const isBodyEmpty = require("../../middlewares");
 const schemas = require("../../schemas/contacts");
 
 const contactsRouter = express.Router();
 
 contactsRouter.get("/", contactsController.getAll)
 
-contactsRouter.get("/:id", contactsController.getById)
+// contactsRouter.get("/:id", contactsController.getById)
 
-contactsRouter.post("/", validateBody(schemas.contactAddSchema), contactsController.add)
+// contactsRouter.post("/", isBodyEmpty, validateBody(schemas.contactAddSchema), contactsController.add)
 
-contactsRouter.put("/:id", validateBody(schemas.contactAddSchema), contactsController.updateById)
+// contactsRouter.put("/:id", isBodyEmpty, validateBody(schemas.contactAddSchema), contactsController.updateById)
 
-contactsRouter.delete("/:id", contactsController.deleteById)
+// contactsRouter.delete("/:id", contactsController.deleteById)
 
 module.exports = contactsRouter;
